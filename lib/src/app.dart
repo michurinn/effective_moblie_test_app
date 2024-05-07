12aba2d8ff95696117fcdc80ca3546cfea4dee2b
michurinnn <michurinnn@gmail.com>
2024-05-07 19:34:45 +0300

    chore: package hab been renamed

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,8 +1,14 @@
+import 'package:effective_test_work/api/dio_configurator.dart';
+import 'package:effective_test_work/api/tickets_api.dart';
+import 'package:effective_test_work/src/find_tickets/data/repository/recommends_repository.dart';
+import 'package:effective_test_work/src/find_tickets/domain/converters/recommended_offer_converter.dart';
+import 'package:effective_test_work/src/find_tickets/presentation/state_managers/recommends_bloc/recommended_offers_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:mad_test_work/src/error_page.dart';
-import 'package:mad_test_work/src/find_tickets/presentation/pages/find_ticket_page.dart';
+import 'package:effective_test_work/src/error_page.dart';
+import 'package:effective_test_work/src/find_tickets/presentation/pages/find_ticket_page.dart';
 
 /// The Widget that configures your application.
 class MyApp extends StatelessWidget {
@@ -33,6 +39,17 @@ class MyApp extends StatelessWidget {
           settings: routeSettings,
           builder: (BuildContext context) {
             switch (routeSettings.name) {
+              case FindTicketsPage.routeName:
+                return BlocProvider(
+                  create: (context) => RecommendedOffersBloc(
+                    recommendedRepository: RecommendedRepository(
+                      ticketsApi: ITicketsApi(AppDioConfigurator()
+                          .create(interceptors: [], url: '')),
+                      converter: RecommendedOfferConverter(),
+                    ),
+                  )..add(GetRecommednedOffersEvent()),
+                  child: const FindTicketsPage(),
+                );
               default:
                 return const ErrorPageView();
             }
--- lib/src/find_tickets/presentation/pages/find_ticket_page.dart
+++ lib/src/find_tickets/presentation/pages/find_ticket_page.dart
@@ -1,11 +1,72 @@
-import 'package:flutter/foundation.dart';
-import 'package:flutter/widgets.dart';
+import 'package:effective_test_work/core/extensions/string_exttensions.dart';
+import 'package:effective_test_work/src/find_tickets/presentation/state_managers/recommends_bloc/recommended_offers_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shimmer/shimmer.dart';
 
 class FindTicketsPage extends StatelessWidget {
   const FindTicketsPage({super.key});
-
+  static const String routeName = '/find-tickets';
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Text('Поиск дешевых авиабилетов'.hardcoded),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Text('Музыкально отлететь'.hardcoded),
+            BlocBuilder<RecommendedOffersBloc, RecommendedOffersState>(
+              builder: (context, state) {
+                return switch (state) {
+                  Empty() => Text('Empty'.hardcoded),
+                  Loading() => Shimmer(
+                      gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [
+                          Colors.grey.shade300,
+                          Colors.grey.shade400,
+                        ],
+                      ),
+                      child: ConstrainedBox(
+                        constraints:
+                            BoxConstraints.tight(const Size.fromHeight(50)),
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: const [
+                            SizedBox.square(
+                              dimension: 50,
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  Loaded(:final offers) => ConstrainedBox(
+                      constraints:
+                          BoxConstraints.tight(const Size.fromHeight(50)),
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: offers
+                            .map(
+                              (e) => SizedBox(
+                                height: 50,
+                                width: 250,
+                                child: Text(e.title),
+                              ),
+                            )
+                            .toList(),
+                      ),
+                    ),
+                  Error() => Text('Error'.hardcoded),
+                };
+              },
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
