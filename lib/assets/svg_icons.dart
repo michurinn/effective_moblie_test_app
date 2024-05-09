2751fef2571b265db1a01dc8fcbe4ca96b56ea83
michurinnn <michurinnn@gmail.com>
2024-05-09 08:46:15 +0300

    feat: resourses have been added

---
+++ lib/assets/svg_icons.dart
@@ -0,0 +1,18 @@
+part of 'resources.dart';
+
+class SvgIcons {
+  SvgIcons._();
+
+  static const String airplaneIcon = 'assets/svg/airplane.svg';
+  static const String hotelBedIcon = 'assets/svg/hotel.svg';
+  static const String mapPointIcon = 'assets/svg/map.svg';
+  static const String profileIcon = 'assets/svg/profile.svg';
+  static const String ringIcon = 'assets/svg/ring.svg';
+  static const String hotSaleIcon = 'assets/svg/burn.svg';
+  static const String weekendIcon = 'assets/svg/calendar.svg';
+  static const String searchingIcon = 'assets/svg/search.svg';
+  static const String complexRouteIcon = 'assets/svg/complex_route.svg';
+  static const String swopeIcon = 'assets/svg/swope_arrows.svg';
+  static const String everythereIcon = 'assets/svg/planet.svg';
+  static const String arrivalPlaneIcon = 'assets/svg/taking_off_plane.svg';
+}
--- lib/src/colors/app_colors.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const listTileBackgroundColor = Colors.white;
-  static const customTextSecond = Color(0xff6B6C6C);
-  static const menuActive = Color(0xff4A6FF4);
-  static const background = Color(0xffEEF2FF);
-}
